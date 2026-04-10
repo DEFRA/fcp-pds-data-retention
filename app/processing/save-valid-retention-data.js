d03eaab597521eff2a0105d07048d924782b2f08
John Barnard <john.barnard.external@eviden.com>
2026-04-10 09:54:43 +0100

    corrected implementation of save valid retention data to return
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/processing/save-valid-retention-data.js
+++ app/processing/save-valid-retention-data.js
@@ -1,7 +1,7 @@
 const db = require('../data')
 
 const saveValidRetentionData = async (validRetentionData) => {
-  await db.retentionData.bulkCreate(validRetentionData)
+  return db.retentionData.bulkCreate(validRetentionData)
 }
 
 module.exports = {
