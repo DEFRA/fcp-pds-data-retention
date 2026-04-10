0494f746bc62be1cdacec9752de6851d8c0093fa
John Barnard <john.barnard.external@eviden.com>
2026-04-10 11:46:40 +0100

    return server and updated healthy test
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/server/start.js
+++ app/server/start.js
@@ -3,6 +3,7 @@ const { createServer } = require('./create-server')
 const start = async () => {
   const server = await createServer()
   await server.start()
+  return server
 }
 
 module.exports = {
