561faa5046e05a4d558e1fa8cc39f489eefbfdf5
Sam Plackett <samuel.plackett@eviden.com>
2026-04-15 16:58:01 +0100

    remove console log

--- app/publishing/get-pending-retention-data.js
+++ app/publishing/get-pending-retention-data.js
@@ -6,7 +6,6 @@ const publishingLimit = 1000
 const getPendingRetentionData = async () => {
   const retentionYearsAgo = new Date()
   retentionYearsAgo.setFullYear(retentionYearsAgo.getFullYear() - retentionYears)
-  console.log(retentionYearsAgo)
   return db.retentionData.findAll({
     where: {
       endDate: { [db.Sequelize.Op.lt]: retentionYearsAgo }
