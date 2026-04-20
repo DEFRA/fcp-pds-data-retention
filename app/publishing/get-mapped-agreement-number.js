580ce90b02a57741661be22322c3e1902e376f0c
Sam Plackett <samuel.plackett@eviden.com>
2026-04-20 16:21:08 +0100

    sonar

--- app/publishing/get-mapped-agreement-number.js
+++ app/publishing/get-mapped-agreement-number.js
@@ -1,5 +1,7 @@
 const { SFI, CS, COHT_CAPITAL, COHT_REVENUE, DELINKED, LUMP_SUMS, SFI_PILOT, SFI23, SFI_EXPANDED } = require('../constants/schemes')
 
+const fullAgreementLength = 8
+
 // The data provided gives us agreement number in a numeric form, e.g. 123456
 // However, the data supplied by D365 is usually appended with a letter relevant to the scheme, e.g. L123456.
 // In addition, some data where there are leading 0s is simplified to numeric form.
@@ -7,21 +9,21 @@ const getMappedAgreementNumber = (schemeId, agreementNumber) => {
   const stringifiedAgreement = String(agreementNumber)
   switch (schemeId) {
     case SFI:
-      return stringifiedAgreement.padStart(8, '0')
+      return stringifiedAgreement.padStart(fullAgreementLength, '0')
     case CS:
-      return 'C' + stringifiedAgreement.padStart(7, '0')
+      return 'C' + stringifiedAgreement.padStart(fullAgreementLength, '0')
     case COHT_REVENUE:
     case SFI_EXPANDED:
-      return 'E' + stringifiedAgreement.padStart(7, '0')
+      return 'E' + stringifiedAgreement.padStart(fullAgreementLength - 1, '0')
     case COHT_CAPITAL:
-      return 'H' + stringifiedAgreement.padStart(7, '0')
+      return 'H' + stringifiedAgreement.padStart(fullAgreementLength - 1, '0')
     case DELINKED:
     case SFI23:
-      return 'Z' + stringifiedAgreement.padStart(7, '0')
+      return 'Z' + stringifiedAgreement.padStart(fullAgreementLength - 1, '0')
     case LUMP_SUMS:
-      return 'L' + stringifiedAgreement.padStart(7, '0')
+      return 'L' + stringifiedAgreement.padStart(fullAgreementLength - 1, '0')
     case SFI_PILOT:
-      return 'S' + stringifiedAgreement.padStart(7, '0')
+      return 'S' + stringifiedAgreement.padStart(fullAgreementLength - 1, '0')
     default:
       return stringifiedAgreement
   }
