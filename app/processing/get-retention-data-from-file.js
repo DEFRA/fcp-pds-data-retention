6005e961727609a9780249fcabd546b11547333b
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2026-05-22 12:28:40 +0100

    Add CPU and memory, fix issue with end date format (#2)

--- app/processing/get-retention-data-from-file.js
+++ app/processing/get-retention-data-from-file.js
@@ -1,5 +1,26 @@
 const csv = require('csv-parser')
 
+const parseDateString = (dateStr) => {
+  if (!dateStr) {
+    return null
+  }
+
+  const [datePart, timePart] = dateStr.split(' ')
+  if (!datePart || !timePart) {
+    return null
+  }
+
+  const [month, day, year] = datePart.split('/')
+  if (!month || !day || !year) {
+    return null
+  }
+
+  const isoString = `${year.padStart(4, '0')}-${month.padStart(2, '0')}-${day.padStart(2, '0')}T${timePart}`
+
+  const date = new Date(isoString)
+  return Number.isNaN(date.getTime()) ? null : date
+}
+
 const getRetentionDataFromFile = (fileStream) => {
   const results = []
   return new Promise((resolve, reject) => {
@@ -10,7 +31,7 @@ const getRetentionDataFromFile = (fileStream) => {
           frn: row['FRN'],
           scheme: row['SCHEME'],
           agreementNumber: row['APP_REF'],
-          endDate: row['APP_END_DATE']
+          endDate: parseDateString(row['APP_END_DATE'])
         })
       })
       .on('end', () => resolve(results))
