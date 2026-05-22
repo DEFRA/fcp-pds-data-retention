7ec1f94ac880168e0fb421b9ad4281a3ba505d49
Sam Plackett <samuel.plackett@eviden.com>
2026-05-22 14:48:37 +0100

    add console logs for debug

--- app/processing/parse-retention-file.js
+++ app/processing/parse-retention-file.js
@@ -14,7 +14,7 @@ const parseRetentionFile = async (filename, fileStream) => {
       const mapped = mapRetentionData([row])
       successfulBatch.push(...mapped.successful)
       unsuccessfulBatch.push(...mapped.unsuccessful)
-
+      console.log(`Parsed row with frn ${row.frn}, scheme ${row.schemeId}`)
       if (successfulBatch.length + unsuccessfulBatch.length >= BATCH_SIZE) {
         await handleParsedRetentionData({
           successful: successfulBatch.splice(0),
