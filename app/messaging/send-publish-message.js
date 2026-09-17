b8e71587098b6f250f0db878149cf0e3ef17809a
Leigh Godson <leigh.godson@atos.net>
2026-09-17 09:35:06 +0100

    replace ffc-messaging with azure service bus native

--- app/messaging/send-publish-message.js
+++ app/messaging/send-publish-message.js
@@ -1,29 +1,20 @@
-const { MessageSender } = require('ffc-messaging')
 const { messageConfig } = require('../config')
+const { getSender, sendMessage: sendServiceBusMessage } = require('./service-bus')
 const { RETENTION_DATA_EXPIRED } = require('../constants/events')
 const { SOURCE } = require('../constants/source')
 
 const sendPublishMessage = async (body) => {
-  let sender
   try {
     const message = {
       body,
       type: RETENTION_DATA_EXPIRED,
       source: SOURCE
     }
-    sender = new MessageSender(messageConfig.retentionTopic)
-    await sender.sendMessage(message)
+    const sender = getSender(messageConfig.retentionTopic)
+    await sendServiceBusMessage(sender, message)
   } catch (error) {
     console.error('Error sending publish message:', error)
     throw error
-  } finally {
-    if (sender) {
-      try {
-        await sender.closeConnection()
-      } catch (closeError) {
-        console.error('Error closing message sender connection:', closeError)
-      }
-    }
   }
 }
 
